bfc65dc3524ad84a49312e8ed37521ac3a741fe3
zahir535 <zahirco535@gmail.com>
2024-05-16 13:40:00 +0800

    react-native-worklets-core setup

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["react-native-reanimated/plugin"], ["react-native-worklets-core/plugin"]],
+  plugins: [["react-native-worklets-core/plugin"], ["react-native-reanimated/plugin", { processNestedWorklets: true }]],
 };
